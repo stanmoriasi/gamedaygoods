b34cb6999862353cb0b5616ae78628e10c7b2e9c
stanmoriasi <stanmoriasi@yahoo.com>
2025-04-11 13:08:44 -0600

    display sample product on home page

---
+++ client/src/components/ProductList/index.tsx
@@ -0,0 +1,55 @@
+// Import `<Link>` component from React Router for internal hyperlinks
+import { Link } from 'react-router-dom';
+
+interface Product {
+  _id: string;
+  productName: string;
+  category: string;
+  description: string;
+  price: number;
+  quantity: number;
+  createdAt: string;
+
+}
+
+interface ProductListProps {
+    products: Product[];
+    productName: string;
+    
+}
+
+const ProductList: React.FC<ProductListProps> = ({ products, productName }) => {
+  
+  if (!products.length) {
+    return <h3>No products Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{productName}</h3>
+      {products &&
+        products.map((product) => (
+          <div key={product._id} className="card mb-3">
+            <h4 className="card-header bg-primary text-light p-2 m-0">
+              {product.productName} <br />
+              <span style={{ fontSize: '1rem' }}>
+                had this product on {new Date(Number(product.createdAt)).toLocaleString()}
+              </span>
+            </h4>
+            <div className="card-body bg-light p-2">
+              <p>{product.description}</p>
+            </div>
+            {/* Create a link to this product's page to view its comments using `<Link>` component */}
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/products/${product._id}`}
+            >
+              Join the discussion on this thought.
+            </Link>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ProductList;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,30 +1,33 @@
 import { useQuery } from '@apollo/client';
-
-import ThoughtList from '../components/ThoughtList/index.tsx';
-import ThoughtForm from '../components/ThoughtForm/index.tsx';
-
-import { QUERY_THOUGHTS } from '../utils/queries.ts';
+import ProductList from '../components/ProductList';
+import { QUERY_PRODUCTS } from '../utils/queries.ts';
+//import ProductList from '../components/ProductList/index.tsx';
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_THOUGHTS);
-  const thoughts = data?.thoughts || [];
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+  
+  const products = data?.products || [];
+  console.log("🚀 ~ Home ~ data:", data)
 
   return (
     <main>
       <div className="flex-row justify-center">
-        <div
+      {/* <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}
         >
-          <ThoughtForm />
-        </div>
+          <ProductList
+            products={products}
+            productName="Default Product Name"
+          />
+        </div> */}
         <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
           ) : (
-            <ThoughtList
-              thoughts={thoughts}
-              title="Some Feed for Thought(s)..."
+            <ProductList
+              products={products}
+              productName="Available Products"
             />
           )}
         </div>
