171d288ad47be8b0b0071ce0e8afe32a89f8b221
stanmoriasi <stanmoriasi@yahoo.com>
2025-04-11 13:03:45 -0600

    update sample seed data

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,9 +1,10 @@
 import db from '../config/connection.js';
-import { Thought, User } from '../models/index.js';
+import { Thought, User, Product } from '../models/index.js';
 import cleanDB from './cleanDB.js';
 
 import userData from './userData.json' with { type: 'json'};
 import thoughtData from './thoughtData.json' with { type: 'json' };
+import productData from './productData.json' with { type: 'json' };
 
 const seedDatabase = async (): Promise<void> => {
   try {
@@ -12,6 +13,7 @@ const seedDatabase = async (): Promise<void> => {
 
     await Thought.insertMany(thoughtData);
     await User.create(userData);
+    await Product.insertMany(productData);
     console.log('Seeding completed successfully!');
     process.exit(0);
   } catch (error) {
