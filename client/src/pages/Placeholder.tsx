07daf9df4a52ee2b8b0d765287ca3fe99411a576
Aeralyn <Aeralyn123@gmail.com>
2025-04-14 12:36:09 -0600

    renaming starter code file

--- client/src/pages/Placeholder.tsx
+++ client/src/pages/Placeholder.tsx
@@ -1,10 +1,12 @@
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+// We can tweak this page to have we might need
 
-import CommentList from '../components/CommentList/index.tsx';
-import CommentForm from '../components/CommentForm/index.tsx';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import { QUERY_SINGLE_THOUGHT } from '../utils/queries.ts';
+import CommentList from "../components/CommentList/index.tsx";
+import CommentForm from "../components/CommentForm/index.tsx";
+
+import { QUERY_SINGLE_THOUGHT } from "../utils/queries.ts";
 
 const SingleThought = () => {
   const { thoughtId } = useParams();
@@ -22,18 +24,19 @@ const SingleThought = () => {
     <div className="my-3">
       <h3 className="card-header bg-dark text-light p-2 m-0">
         {thought.thoughtAuthor} <br />
-        <span style={{ fontSize: '1rem' }}>
-          had this thought on {new Date(Number(thought.createdAt)).toLocaleString()}
+        <span style={{ fontSize: "1rem" }}>
+          had this thought on{" "}
+          {new Date(Number(thought.createdAt)).toLocaleString()}
         </span>
       </h3>
       <div className="bg-light py-4">
         <blockquote
           className="p-4"
           style={{
-            fontSize: '1.5rem',
-            fontStyle: 'italic',
-            border: '2px dotted #1a1a1a',
-            lineHeight: '1.5',
+            fontSize: "1.5rem",
+            fontStyle: "italic",
+            border: "2px dotted #1a1a1a",
+            lineHeight: "1.5",
           }}
         >
           {thought.thoughtText}
@@ -43,7 +46,7 @@ const SingleThought = () => {
       <div className="my-5">
         <CommentList comments={thought.comments} />
       </div>
-      <div className="m-3 p-4" style={{ border: '1px dotted #1a1a1a' }}>
+      <div className="m-3 p-4" style={{ border: "1px dotted #1a1a1a" }}>
         <CommentForm thoughtId={thought._id} />
       </div>
     </div>
