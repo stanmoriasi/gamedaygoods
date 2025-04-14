a2b77283e2880f3357e53d084d5b9b46a1b5cc69
Aeralyn <Aeralyn123@gmail.com>
2025-04-14 16:44:59 -0600

    resolving conflict

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import SingleThought from "./pages/Placeholder.js";
+import SingleThought from "./pages/SingleThought";
 import Profile from "./pages/Profile";
 import ErrorPage from "./pages/Error";
 
