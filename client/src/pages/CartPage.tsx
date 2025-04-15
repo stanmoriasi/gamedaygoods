f810842587d3288b32730ea3f46acaa46e6b7ca7
stanmoriasi <stanmoriasi@yahoo.com>
2025-04-15 11:22:51 -0600

    Add changes to view cart, and add to cart

--- client/src/pages/CartPage.tsx
+++ client/src/pages/CartPage.tsx
@@ -1,20 +1,5 @@
-import { useQuery, useMutation, gql } from '@apollo/client';
-
-const GET_CART = gql`
-  query GetCart {
-    cart {
-      _id
-      products {
-        _id
-        productName
-        price
-        description
-      }
-      total
-      createdAt
-    }
-  }
-`;
+import { useMutation, gql } from '@apollo/client';
+import { useEffect, useState } from 'react';
 
 const PLACE_ORDER = gql`
   mutation PlaceOrder($input: [ID!]!) {
@@ -29,42 +14,53 @@ const PLACE_ORDER = gql`
   }
 `;
 
+interface cartItem {
+  _id: string, 
+  quantity: number, 
+  productName: string, 
+  price: number
+}
+
 const CartPage = () => {
-  const { loading, error, data } = useQuery(GET_CART);
   const [placeOrder] = useMutation(PLACE_ORDER);
+  const [total, setTotal] = useState(0);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error loading cart: {error.message}</p>;
+  const storedItems = localStorage.getItem('cart');
+  const cart = storedItems ? JSON.parse(storedItems) : [];
 
-  const cart = data?.cart;
-  const productIds = cart?.products.map((p: any) => p._id);
+  useEffect(()=> {
+    setTotal(cart.reduce((sum: number, b: cartItem) => sum + (b.price * b.quantity), 0))
+  }, [cart])
+
+  const productIds = cart?.map((p: any) => p._id);
 
   const handleCheckout = async () => {
-    try {
-      await placeOrder({ variables: { input: productIds } });
-      alert('Order placed!');
-    } catch (err) {
-      console.error(err);
-      alert('Failed to place order.');
-    }
+    console.log(cart)
+    // try {
+    //   await placeOrder({ variables: { input: productIds } });
+    //   alert('Order placed!');
+    // } catch (err) {
+    //   console.error(err);
+    //   alert('Failed to place order.');
+    // }
   };
 
   return (
     <div className="container">
       <h2>Your Cart</h2>
-      {cart.products.length === 0 ? (
+      {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
         <>
           <ul>
-            {cart.products.map((product: any) => (
+            {cart.map((product: any) => (
               <li key={product._id}>
                 <h4>{product.productName}</h4>
                 <p>${product.price}</p>
               </li>
             ))}
           </ul>
-          <h3>Total: ${cart.total.toFixed(2)}</h3>
+          <h3>Total: ${total?.toFixed(2)}</h3>
           <button onClick={handleCheckout} className="btn btn-success">
             Checkout
           </button>
