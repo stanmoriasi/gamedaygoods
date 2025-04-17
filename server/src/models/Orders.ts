3e52c8865ac0e5e77c231bacc6efe2e2e4acd363
stanmoriasi <stanmoriasi@yahoo.com>
2025-04-16 18:51:45 -0600

    update Order model/schema

--- server/src/models/Orders.ts
+++ server/src/models/Orders.ts
@@ -1,13 +1,19 @@
-import { Schema, model, Document } from 'mongoose';
+import mongoose, { Schema, model, Document, Types } from 'mongoose';
 
 interface IOrder extends Document {
-  userId: string;
+  userId: Types.ObjectId;
   products: {
-    productId: string;
+    productId: Types.ObjectId;
     quantity: number;
   }[];
   amount: number;
-  address: string;
+  address: {
+    street: string
+    city: string
+    state: string
+    zipCode: string
+    country: string
+  };
   status: string;
   createdAt: Date;
 }
@@ -15,8 +21,9 @@ interface IOrder extends Document {
 const orderSchema = new Schema<IOrder>(
   {
     userId: {
-      type: String,
-      required: false,
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
     },
     products: [
       {
@@ -38,13 +45,19 @@ const orderSchema = new Schema<IOrder>(
       default: 0,
     },
     address: {
-      type: String,
+      type: {
+        street: String,
+        city: String,
+        state: String,
+        zipCode: String,
+        country: String
+      },
       required: false,
     },
     status: {
       type: String,
-      enum: ['pending', 'shipped', 'delivered', 'cancelled', 'cart'],
-      default: 'cart',
+      enum: ['pending', 'shipped', 'delivered', 'cancelled'],
+      default: 'pending',
     },
     createdAt: {
       type: Date,
@@ -52,6 +65,26 @@ const orderSchema = new Schema<IOrder>(
     },
   }
 );
+
+orderSchema.pre<IOrder>('save', async function (next) {
+  const productIds = this.products.map((item) => item.productId);
+  const products = await mongoose.model('Product').find({ _id: { $in: productIds } });
+
+  let total = 0;
+  for (const item of this.products) {
+    const product = products.find((p) =>  p._id.toString().includes(item.productId));
+    if (!product) {
+      return next(new Error(`Product ${item.productId} not found`));
+    }
+    if (item.quantity > product.quantity) {
+      return next(new Error(`Only ${product.quantity} ${product.productName}(s) in stock.`));
+    }
+    total += item.quantity * product.price;
+  }
+
+  this.amount = total;
+  next();
+});
+
 const Order = model<IOrder>('Order', orderSchema);
 export default Order;
-
